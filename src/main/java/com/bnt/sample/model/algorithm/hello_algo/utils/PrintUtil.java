a458dcfe021058a55164867052ad735e3555f7b4
bnt <1668629432@qq.com>
2024-03-19 22:51:55 +0800

    邻接矩阵实现图

--- src/main/java/com/bnt/sample/model/algorithm/hello_algo/utils/PrintUtil.java
+++ src/main/java/com/bnt/sample/model/algorithm/hello_algo/utils/PrintUtil.java
@@ -106,4 +106,22 @@ public class PrintUtil {
         TreeNode root = TreeNode.listToTree(list);
         printTree(root);
     }
+
+    /* Print a matrix (Array) */
+    public static <T> void printMatrix(T[][] matrix) {
+        System.out.println("[");
+        for (T[] row : matrix) {
+            System.out.println("  " + row + ",");
+        }
+        System.out.println("]");
+    }
+
+    /* Print a matrix (List) */
+    public static <T> void printMatrix(List<List<T>> matrix) {
+        System.out.println("[");
+        for (List<T> row : matrix) {
+            System.out.println("  " + row + ",");
+        }
+        System.out.println("]");
+    }
 }
