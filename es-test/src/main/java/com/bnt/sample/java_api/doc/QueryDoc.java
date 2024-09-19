102ddb41214f20d73915ed934e0434ba9df571e8
“bnt” <abuwyyx@163.com>
2024-09-19 08:00:00 +0800

    高亮，范围，分页，模糊，组合查询

--- es-test/src/main/java/com/bnt/sample/java_api/doc/QueryDoc.java
+++ es-test/src/main/java/com/bnt/sample/java_api/doc/QueryDoc.java
@@ -5,13 +5,16 @@ import com.bnt.sample.java_api.ElasticsearchTask;
 import org.elasticsearch.action.search.SearchRequest;
 import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.client.RequestOptions;
-import org.elasticsearch.index.query.QueryBuilder;
-import org.elasticsearch.index.query.QueryBuilders;
-import org.elasticsearch.index.query.TermQueryBuilder;
+import org.elasticsearch.common.unit.Fuzziness;
+import org.elasticsearch.index.query.*;
 import org.elasticsearch.search.SearchHit;
 import org.elasticsearch.search.SearchHits;
 import org.elasticsearch.search.builder.SearchSourceBuilder;
+import org.elasticsearch.search.fetch.subphase.highlight.HighlightBuilder;
+import org.elasticsearch.search.fetch.subphase.highlight.HighlightField;
 import org.elasticsearch.search.sort.SortOrder;
+
+import java.util.Map;
 
 /**
  * 全量查询
@@ -108,10 +111,110 @@ public class QueryDoc {
         System.out.println("<<========");
     };
 
+    private static final ElasticsearchTask SEARCH_BY_CONDITION2 = client -> {
+        SearchRequest searchRequest = new SearchRequest();
+        searchRequest.indices("user");
+        SearchSourceBuilder searchSourceBuilder = new SearchSourceBuilder();
+        BoolQueryBuilder boolQueryBuilder = QueryBuilders.boolQuery();
+        boolQueryBuilder.must(QueryBuilders.matchQuery("age","30"));
+        boolQueryBuilder.mustNot(QueryBuilders.matchQuery("name","zhangsan"));
+        boolQueryBuilder.should(QueryBuilders.matchQuery("sex","男"));
+        searchSourceBuilder.query(boolQueryBuilder);
+        searchRequest.source(searchSourceBuilder);
+        SearchResponse response = client.search(searchRequest, RequestOptions.DEFAULT);
+        SearchHits hits = response.getHits();
+        System.out.println("took:" + response.getTook());
+        System.out.println("timeout:" + response.isTimedOut());
+        System.out.println("total:" + hits.getTotalHits());
+        System.out.println("MaxScore:" + hits.getMaxScore());
+        System.out.println("hits========>>");
+        for (SearchHit hit : hits) {
+            //输出每条查询的结果信息
+            System.out.println(hit.getSourceAsString());
+        }
+        System.out.println("<<========");
+    };
+
+    private static final ElasticsearchTask SEARCH_BY_RANGE = client -> {
+        SearchRequest searchRequest = new SearchRequest();
+        searchRequest.indices("user");
+        SearchSourceBuilder searchSourceBuilder = new SearchSourceBuilder();
+        RangeQueryBuilder rangeQueryBuilder = QueryBuilders.rangeQuery("age");
+        rangeQueryBuilder.gte("30");
+        rangeQueryBuilder.lt("40");
+        searchSourceBuilder.query(rangeQueryBuilder);
+        searchRequest.source(searchSourceBuilder);
+        SearchResponse response = client.search(searchRequest, RequestOptions.DEFAULT);
+        SearchHits hits = response.getHits();
+        System.out.println("took:" + response.getTook());
+        System.out.println("timeout:" + response.isTimedOut());
+        System.out.println("total:" + hits.getTotalHits());
+        System.out.println("MaxScore:" + hits.getMaxScore());
+        System.out.println("hits========>>");
+        for (SearchHit hit : hits) {
+            //输出每条查询的结果信息
+            System.out.println(hit.getSourceAsString());
+        }
+        System.out.println("<<========");
+    };
+
+    private static final ElasticsearchTask SEARCH_BY_FUZZY_CONDITION = client -> {
+        SearchRequest searchRequest = new SearchRequest();
+        searchRequest.indices("user");
+        SearchSourceBuilder searchSourceBuilder = new SearchSourceBuilder();
+        FuzzyQueryBuilder fuzziness = QueryBuilders.fuzzyQuery("name", "wangwu").fuzziness(Fuzziness.ONE);
+        searchSourceBuilder.query(fuzziness);
+        searchRequest.source(searchSourceBuilder);
+        SearchResponse response = client.search(searchRequest, RequestOptions.DEFAULT);
+        SearchHits hits = response.getHits();
+        System.out.println("took:" + response.getTook());
+        System.out.println("timeout:" + response.isTimedOut());
+        System.out.println("total:" + hits.getTotalHits());
+        System.out.println("MaxScore:" + hits.getMaxScore());
+        System.out.println("hits========>>");
+        for (SearchHit hit : hits) {
+            //输出每条查询的结果信息
+            System.out.println(hit.getSourceAsString());
+        }
+        System.out.println("<<========");
+    };
+
+    private static final ElasticsearchTask SEARCH_WITH_HIGHLIGHT = client -> {
+        SearchRequest searchRequest = new SearchRequest();
+        searchRequest.indices("user");
+        SearchSourceBuilder searchSourceBuilder = new SearchSourceBuilder();
+        TermsQueryBuilder termsQueryBuilder = QueryBuilders.termsQuery("name","zhangsan");
+        searchSourceBuilder.query(termsQueryBuilder);
+        HighlightBuilder highlightBuilder = new HighlightBuilder();
+        highlightBuilder.field("name");
+        highlightBuilder.preTags("<font color='red'>");
+        highlightBuilder.postTags("</font>");
+        searchSourceBuilder.highlighter(highlightBuilder);
+        searchRequest.source(searchSourceBuilder);
+        SearchResponse response = client.search(searchRequest, RequestOptions.DEFAULT);
+        SearchHits hits = response.getHits();
+        System.out.println("took:" + response.getTook());
+        System.out.println("timeout:" + response.isTimedOut());
+        System.out.println("total:" + hits.getTotalHits());
+        System.out.println("MaxScore:" + hits.getMaxScore());
+        System.out.println("hits========>>");
+        for (SearchHit hit : hits) {
+            //输出每条查询的结果信息
+            System.out.println(hit.getSourceAsString());
+            Map<String, HighlightField> highlightFields = hit.getHighlightFields();
+            System.out.println(highlightFields);
+        }
+        System.out.println("<<========");
+    };
+
     public static void main(String[] args) {
         ConnectElasticsearch.connect(SEARCH_MATCHALL_QUERY);
         ConnectElasticsearch.connect(SEARCH_BY_CONDITION);
         ConnectElasticsearch.connect(SEARCH_BY_PAGING);
         ConnectElasticsearch.connect(SEARCH_WITH_ORDER);
+        ConnectElasticsearch.connect(SEARCH_BY_CONDITION2);
+        ConnectElasticsearch.connect(SEARCH_BY_RANGE);
+        ConnectElasticsearch.connect(SEARCH_BY_FUZZY_CONDITION);
+        ConnectElasticsearch.connect(SEARCH_WITH_HIGHLIGHT);
     }
 }
