04df6dce9fba575bde0db47f251830ac2f9ede04
bnt <49509592+bu0207@users.noreply.github.com>
2024-10-29 18:18:28 +0800

    es springboot集成

---
+++ es-springboot-test/src/main/java/com/bnt/sample/esspringboottest/EsSpringbootTestApplication.java
@@ -0,0 +1,13 @@
+package com.bnt.sample.esspringboottest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsSpringbootTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EsSpringbootTestApplication.class, args);
+    }
+
+}
